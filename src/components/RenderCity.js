c21f0d746f40c04194775faa38bf84a887b99aff
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-06-14 10:18:02 +0300

    some CSS added

--- src/components/RenderCity.js
+++ src/components/RenderCity.js
@@ -8,12 +8,12 @@ export class RenderCity extends Component {
       <div>
         {
           this.props.show &&
-          <div>
-            <Alert variant='success'>
-              {this.props.data.display_name}, {this.props.data.lat}, {this.props.data.lon}
+          <div> 
+            <Alert variant='success'  id='dataAlert'>
+              City Name : {this.props.data.display_name} || Location : {this.props.data.lat}, {this.props.data.lon}
             </Alert>
 
-            <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.65682c34878313a764272bb30d845031&center=${this.props.data.lat},${this.props.data.lon}&zoom=13&size=1000x300`} fluid center />
+            <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.65682c34878313a764272bb30d845031&center=${this.props.data.lat},${this.props.data.lon}&zoom=13&size=1500x250`} fluid center />
           </div>
         }
         {
